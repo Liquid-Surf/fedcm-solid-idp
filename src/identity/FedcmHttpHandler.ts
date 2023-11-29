d0a66a69e688c5ebff30f1669c709de754165bb2
thhck <thhck@proton.me>
2023-11-29 20:51:18 +0100

    Add baseUrl in FedcmHttpHandler +basic placeholder

--- src/identity/FedcmHttpHandler.ts
+++ src/identity/FedcmHttpHandler.ts
@@ -3,16 +3,23 @@ import type { HttpHandlerInput } from '../server/HttpHandler';
 import { HttpHandler } from '../server/HttpHandler';
 
 /**
- * HTTP handler that redirects all requests to the OIDC library.
+ * HTTP handler that redirects all requests to the FedCM library.
  */
 export class FedcmHttpHandler extends HttpHandler {
   protected readonly logger = getLoggerFor(this);
 
+  private readonly baseUrl: string;
+
+  public constructor(baseUrl: string) {
+    super();
+    this.baseUrl = baseUrl;
+  }
+
   public async handle({ request, response }: HttpHandlerInput): Promise<void> {
-    const currentDate = JSON.stringify({ date: new Date().toISOString() });
     switch (request.url) {
       case '/.well-known/web-identity':
-        await this.handleWebIdentity({request, response })
+    
+      await this.handleWebIdentity({request, response })
         break;
       case '/.well-known/fedcm/fedcm.json':
         await this.handleFedcmJSON({request, response })
@@ -27,26 +34,55 @@ export class FedcmHttpHandler extends HttpHandler {
       default:
         break;
     }
-    response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.end(currentDate);
+    response.writeHead(500, { 'Content-Type': 'application/json' });
+    response.end(JSON.stringify({'error': { 'message': 'Fail in FedcmHttpHandler to handle the request url.'}}));
   }
 
   
-  async handleWebIdentity({request, response }: HttpHandlerInput): Promise<void> {
+  private async handleWebIdentity({request, response }: HttpHandlerInput): Promise<void> {
+    const providers = [ `${this.baseUrl}/.well-known/fedcm/fedcm.js`]
     response.writeHead(200, { 'Content-Type': 'application/json'})
-    response.end(JSON.stringify({'resp': request.url}))
+    response.end(JSON.stringify({'provider_urls': providers}))
   }
-  async handleFedcmJSON({request, response }: HttpHandlerInput): Promise<void> {
+  private async handleFedcmJSON({request, response }: HttpHandlerInput): Promise<void> {
+    const config = {
+      "accounts_endpoint": "/.well-known/fedcm/accounts_endpoint",
+      "client_metadata_endpoint": "/.well-known/fedcm/client_metadata_endpoint",
+      "id_assertion_endpoint": "/.well-known/fedcm/token_endpoint",
+      "revocation_endpoint": "/.well-known/fedcm/revocation_endpoint",
+      "login_url": "/.account/login/password/",
+      "branding": {
+        "background_color": "rgb(055, 055, 204)",
+        "color": "0xffffff",
+        "icons": [
+          {
+            "url": `${this.baseUrl}/.well-known/css/images/solid.svg`,
+            "size": 32
+          }
+        ]
+      }
+    }
+    
     response.writeHead(200, { 'Content-Type': 'application/json'})
-    response.end(JSON.stringify({'resp': request.url}))
+    response.end(JSON.stringify(config))
   }
-  async handleAccountsEnpoint({request, response }: HttpHandlerInput): Promise<void> {
+  private async handleAccountsEnpoint({request, response }: HttpHandlerInput): Promise<void> {
+    // sessionId = parse(request.cookie)
+    // user = fetchUser(sessionId)
+    // user_accounts = list_accounts(user)
+    const accounts = {
+      'accounts': []
+    }
     response.writeHead(200, { 'Content-Type': 'application/json'})
-    response.end(JSON.stringify({'resp': request.url}))
+    response.end(JSON.stringify(accounts))
   }
-  async handleClientMetadataEndpoint({request, response }: HttpHandlerInput): Promise<void> {
-    response.writeHead(200, { 'Content-Type': 'application/json'})
-    response.end(JSON.stringify({'resp': request.url}))
+  private async handleClientMetadataEndpoint({ response }: HttpHandlerInput): Promise<void> {
+    const metadata = {
+      privacy_policy_url: '...',
+      terms_of_service_url: '...'
+    };
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.end(JSON.stringify(metadata));
   }
 
 }
